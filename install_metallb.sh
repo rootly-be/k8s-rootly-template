86b9805d16781de80fca973571bb88b0a19e493e
rootly <48964029+rootly-be@users.noreply.github.com>
2024-05-25 13:13:05 +0200

    Update install_metallb.sh

--- install_metallb.sh
+++ install_metallb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+sleep 30
 kubectl apply -f https://raw.githubusercontent.com/rootly-be/k8s-rootly-template/main/ipaddresspool.yaml
 kubectl apply -f https://raw.githubusercontent.com/rootly-be/k8s-rootly-template/main/layer2advertisement.yaml
